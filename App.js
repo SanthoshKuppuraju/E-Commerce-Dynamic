9ce97c9912cdf33fb2a1a8204b46379c0b93b715
SanthoshKuppuraju <santhoshkuppuraju@gmail.com>
2024-05-22 21:42:32 +0530

    All the files of the project was uploded

---
+++ App.js
@@ -0,0 +1,12 @@
+import Routing from "./Routing";
+import Header from "./Header";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routing />
+    </div>
+  );
+}
+
+export default App;
