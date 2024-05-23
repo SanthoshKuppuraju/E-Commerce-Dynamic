baa43c10f21a2c5534f2bbc2b84d46d6d1d6fdf2
SanthoshKuppuraju <santhoshkuppuraju@gmail.com>
2024-05-23 22:00:08 +0530

    changed into dynamic

--- Home.js
+++ Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Home.css";
-import img1 from "./Images/hero-img.png";
-import img2 from "./Images/phone-04.jpg";
-import img3 from "./Images/phone-06.jpg";
 import Footer from "./Footer";
 import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
+import { SliderData } from "./Product.js";
+import { serviceData } from "./Product.js";
+import { discoutProducts } from "./Product.js";
 
 const Home = () => {
   const [index, setIndex] = useState(0);
@@ -18,48 +18,57 @@ const Home = () => {
     <>
       <div>
         {/* For the carousel part  */}
+
         <Carousel
           className="carousel"
           activeIndex={index}
           onSelect={handleSelect}
         >
-          <Carousel.Item>
-            <Carousel.Caption>
-              <div className="cntofcarousel">
-                <h3>50% off for your first shopping</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-                <button className="buttonofcarousel">Visit collection</button>
-              </div>
-            </Carousel.Caption>
-            <img className="carosalimg" src={img1} alt="carosalimage" />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="carosalimg" src={img2} alt="carosalimage" />
-            <Carousel.Caption>
-              <div className="cntofcarousel">
-                <h3>Grab Your first iphone as 13 pro </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <button className="buttonofcarousel">Visit collection</button>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="carosalimg" src={img3} alt="carosalimage" />
-            <Carousel.Caption>
-              <div className="cntofcarousel">
-                <h3>40% off on Iphone 14 pro max</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-                <button className="buttonofcarousel">Visit collection</button>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {SliderData.map((item) => (
+            <Carousel.Item>
+              <Carousel.Caption>
+                <div className="cntofcarousel">
+                  <h3>{item.title}</h3>
+                  <p>{item.desc}</p>
+                  <button className="buttonofcarousel">Visit collection</button>
+                </div>
+              </Carousel.Caption>
+
+              <img className="carosalimg" src={item.cover} alt="carosalimage" />
+            </Carousel.Item>
+          ))}
         </Carousel>
-        {/* For the cart below the carousel */}
+
+        {/* For the sevices*/}
+        <div className="cardsundercar">
+          {serviceData.map((item) => (
+            <div className="card" style={{ backgroundColor: item.bg }}>
+              <center>
+                <img src={item.icon} />
+                <h5>{item.title} </h5>
+                <p>{item.subtitle}</p>
+              </center>
+            </div>
+          ))}
+        </div>
+
+        {/* Big Discount */}
+        <div className="bigdiscount">
+          <h1>Big Discount</h1>
+          <div className="bgproducts">
+            {discoutProducts.map((item) => (
+              <div className="bgitems">
+                <img src={item.imgUrl} />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/*  */}
+        {/*  */}
+        {/*  */}
+        {/*  */}
+        {/*  */}
         <Footer />
       </div>
     </>
