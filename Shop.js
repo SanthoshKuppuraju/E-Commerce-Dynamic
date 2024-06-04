a0d8f44978e506c07e64467dbc0a55ee4b6d0fbb
SanthoshKuppuraju <santhoshkuppuraju@gmail.com>
2024-06-04 22:13:20 +0530

    Completed Search Bar and added Add to cart message

--- Shop.js
+++ Shop.js
@@ -1,25 +1,50 @@
 import React, { useReducer, useState } from "react";
-import "./Shop.css";
 import { products } from "./Product";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import { useDispatch } from "react-redux";
 import { add } from "./CartSlice";
+import { toast, ToastContainer } from "react-toastify";
+import "./Shop.css";
 
 const Shop = () => {
-  const [data, setData] = useState(products); // inital state of the products
-  // const categories = data.map((item) => ({ category: item.category }));
-  const categories = [{ category: "sofa" }, { category: "mobile" }];
+  const shopProducts = products.filter((item) => item.category == "sofa");
+  const [data, setData] = useState(shopProducts); // inital state of the products
+  // const categories = products.map((item) => ({ category: item.category }));
 
-  const reducer = (currentState, action) => {
-    console.log(action);
+  const categories = [
+    { category: "sofa" },
+    { category: "mobile" },
+    { category: "chair" },
+    { category: "watch" },
+    { category: "wireless" },
+  ];
+
+  // category filter  by the category
+  const reducer = (currentState, action) => {};
+
+  const handelFilter = () => {
+    setData(category);
   };
 
   const [category, dispatch] = useReducer(reducer, data);
+  // filter the data by category
+
   // to dispatch the data while clicking rthe add to cart button
   const dispatchCart = useDispatch(); // to dispatch the product data to the slice
   const handelAdd = (item) => {
     dispatchCart(add(item));
+    toast.success("Product has been added to cart");
+  };
+
+  // for the search bar
+  const [qtyInputValue, setQtyInputValue] = useState("");
+  const searchedProduct = data.filter((item) =>
+    item.productName.toLowerCase().includes(qtyInputValue.toLowerCase())
+  );
+
+  const handleInputChange = (event) => {
+    setQtyInputValue(event.target.value);
   };
 
   return (
@@ -33,22 +58,23 @@ const Shop = () => {
       <select className="catagerydropdown">
         <option> Filter by category </option>
         {categories.map((item) => (
-          <option>{item.category}</option>
+          <option
+            onClick={() => {
+              handelFilter();
+              dispatch({ type: `${item.category.toUpperCase()}` });
+            }}
+          >
+            {item.category}
+          </option>
         ))}
       </select>
-      {/* <button
-        onClick={() => {
-          dispatch({ type: `MOBILE`, payLoad: 0 });
-        }}
-      >
-        Click me{" "}
-      </button> */}
 
-      {/* Search Bar */}
       <div className="searchbar">
         <input
           class="form-control me-2"
           type="search"
+          value={qtyInputValue}
+          onChange={handleInputChange}
           placeholder="Search"
           aria-label="Search"
         />
@@ -62,32 +88,58 @@ const Shop = () => {
       {/* For the Products */}
       <div className="smilarproductscontainer">
         <div className="similarproducts">
-          {category.map((item) => (
-            <div className="similaritems">
-              <Link to={`/productpage/${item.id}`}>
-                <img src={item.imgUrl} className="similarproductsimg" />
-              </Link>
-              <h5>{item.productName}</h5>
-              <div className="similarstarscontainer">
-                <img src={item.star} className="similarproductstar" />
-                <img src={item.star} className="similarproductstar" />
-                <img src={item.star} className="similarproductstar" />
-                <img src={item.star} className="similarproductstar" />
-                <img src={item.star} className="similarproductstar" />
-              </div>
-              <h5>INR: {item.price}</h5>
+          {searchedProduct && searchedProduct.length < 0
+            ? category.map((item) => (
+                <div className="similaritems">
+                  <Link to={`/productpage/${item.id}`}>
+                    <img src={item.imgUrl} className="similarproductsimg" />
+                  </Link>
+                  <h5>{item.productName}</h5>
+                  <div className="similarstarscontainer">
+                    <img src={item.star} className="similarproductstar" />
+                    <img src={item.star} className="similarproductstar" />
+                    <img src={item.star} className="similarproductstar" />
+                    <img src={item.star} className="similarproductstar" />
+                    <img src={item.star} className="similarproductstar" />
+                  </div>
+                  <h5>INR: {item.price}</h5>
 
-              <button
-                onClick={() => {
-                  handelAdd(item);
-                }}
-              >
-                +
-              </button>
-            </div>
-          ))}
+                  <button
+                    onClick={() => {
+                      handelAdd(item);
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
+              ))
+            : searchedProduct.map((item) => (
+                <div className="similaritems">
+                  <Link to={`/productpage/${item.id}`}>
+                    <img src={item.imgUrl} className="similarproductsimg" />
+                  </Link>
+                  <h5>{item.productName}</h5>
+                  <div className="similarstarscontainer">
+                    <img src={item.star} className="similarproductstar" />
+                    <img src={item.star} className="similarproductstar" />
+                    <img src={item.star} className="similarproductstar" />
+                    <img src={item.star} className="similarproductstar" />
+                    <img src={item.star} className="similarproductstar" />
+                  </div>
+                  <h5>INR: {item.price}</h5>
+
+                  <button
+                    onClick={() => {
+                      handelAdd(item);
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
+              ))}
         </div>
       </div>
+      <ToastContainer />
       <Footer />
     </>
   );
