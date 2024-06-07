d0e07967163640a07a32a263cb84957054706423
SanthoshKuppuraju <santhoshkuppuraju@gmail.com>
2024-06-07 19:54:16 +0530

    PROJECT COMPLETED

--- Cart.js
+++ Cart.js
@@ -2,13 +2,23 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Cart.css";
 import Footer from "./Footer";
-import { remove } from "./CartSlice";
+import { remove, incrementQuantity, decrementQuantity } from "./CartSlice";
 
 const Cart = () => {
-  const cartData = useSelector((state) => state.cart);
+  const cartData = useSelector((state) => state.cart.items);
+  const total = useSelector((state) => state.cart.total);
+
   const dispatch = useDispatch();
   const handleRemove = (id) => {
     dispatch(remove(id));
+  };
+
+  const handleIncrement = (id) => {
+    dispatch(incrementQuantity(id));
+  };
+
+  const handleDecrement = (id) => {
+    dispatch(decrementQuantity(id));
   };
 
   return (
@@ -33,6 +43,41 @@ const Cart = () => {
                     />
                   </button>
                 </div>
+                {/* For the quantities */}
+                <div className="quantitysection">
+                  <div className="quantityrates">
+                    <p>
+                      ${item.price} * {item.quantity}
+                    </p>
+                    <p className="quntytotalprice">
+                      ${item.price * item.quantity}
+                    </p>
+                  </div>
+                  <div className="quantitybuttons">
+                    <button
+                      className="quntbutton1"
+                      onClick={() => {
+                        handleIncrement(item.id);
+                      }}
+                    >
+                      <img
+                        src="https://cdn3.iconfinder.com/data/icons/user-interface-169/32/plus-64.png"
+                        alt="addqunty"
+                      />
+                    </button>
+                    <button
+                      className="quntbutton2"
+                      onClick={() => {
+                        handleDecrement(item.id);
+                      }}
+                    >
+                      <img
+                        src="https://cdn4.iconfinder.com/data/icons/liny/24/minus-line-64.png"
+                        alt="minusqunty"
+                      />
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           ))
@@ -50,12 +95,11 @@ const Cart = () => {
       <div className="cartsummary">
         <h4>Cart Summary</h4>
         <p>Total price:</p>
-        <h5> INR: 1111.00</h5>
+        <h5> ${total}.00</h5>
       </div>
 
       <Footer />
     </>
   );
 };
-
 export default Cart;
--- CartSlice.js
+++ CartSlice.js
@@ -1,19 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const CartSlice = createSlice({
+const initialState = {
+  items: [],
+  total: 0,
+};
+
+const cartSlice = createSlice({
   name: "cart",
-  initialState: [],
+  initialState,
   reducers: {
     add(state, action) {
-      state.push(action.payload);
-      console.log(action.payload);
+      const item = action.payload;
+      const existingItem = state.items.find((itm) => itm.id === item.id);
+      if (existingItem) {
+        existingItem.quantity += 1; // for the clickimg the same product again and again if the product exists
+      } else {
+        state.items.push({ ...item, quantity: 1 }); // if the item occurs only one time we just add the item to the cart and the default quantity is 1
+      }
+      state.total += item.price; // once the product is added into the cart we add that price of the product
     },
     remove(state, action) {
-      return state.filter((item) => item.id !== action.payload);
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      ); // find the index of the item and remove that item from the cart
+      if (itemIndex >= 0) {
+        state.total -=
+          state.items[itemIndex].price * state.items[itemIndex].quantity;
+        state.items.splice(itemIndex, 1); // using splice method we remove the item
+      }
+    },
+    incrementQuantity(state, action) {
+      const item = state.items.find((item) => item.id === action.payload); // by teh id we are going to add the particular item's qunty
+      if (item) {
+        item.quantity += 1;
+        state.total += item.price; // and add the price to the total again once they clicked the increment button
+      }
+    },
+    decrementQuantity(state, action) {
+      const item = state.items.find((item) => item.id === action.payload); // as same as increment we are going to  minus thet quantity by the id
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+        state.total -= item.price; // annd minus the total by the price of the product
+      }
     },
   },
 });
 
-export const { add, remove } = CartSlice.actions;
+export const { add, remove, incrementQuantity, decrementQuantity } =
+  cartSlice.actions;
 
-export default CartSlice.reducer;
+export default cartSlice.reducer;
